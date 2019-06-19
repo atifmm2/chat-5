f0c22b3a7a46017ccea6fe7dfb92ad7012993c6d
amahmood <atif.mahmood@helsinki.fi>
2019-06-19 18:14:15 +0300

    check the q4-linear

--- q4-linear.f90
+++ q4-linear.f90
@@ -2,31 +2,24 @@ program q4
 implicit none
 
 character, dimension(5) :: string
-character :: res
 character :: smallest
+character :: res
 string  = (/"e","a","i","o","u"/)
 
-res =  smallest(string)
-print*, res
+res = smallest(string)
+print*,res
 end program q4
 
 character function smallest(string) result (value)
 
 character, dimension(5) :: string
-! character :: value
-integer :: i, j
-logical :: is_smallest
-do i=1,5
-  is_smallest= .true.
-  do j = 1,5
-    if (i == j) continue
+character :: small_ans
+integer :: i
 
-    if (string (j) < string (i)) then
-      is_smallest = .false.
-    end if
-  end do
-  if (is_smallest .eqv. .true.) then
-  value = string(i)
+small_ans = string (1)
+do i=1,5
+  if ( small_ans > string (i)) then
+    small_ans = string(i)
   end if
 end do
 
